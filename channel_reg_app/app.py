25ab362357decb7c14aa4936f58a9fa16b52dbb4
kkaris <karis.klas@gmail.com>
2020-02-12 17:32:38 -0500

    Use list in sess, redirect, if-else index template

--- channel_reg_app/app.py
+++ channel_reg_app/app.py
@@ -1,10 +1,11 @@
 from flask_wtf import Form
 from flask_bootstrap import Bootstrap
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, session
 from wtforms import SubmitField, StringField, validators, SelectMultipleField
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'dev_key'
+
 Bootstrap(app)
 
 
@@ -13,9 +14,11 @@ channels = ['A', 'B', 'C']
 
 class ChannelSearchForm(Form):
     inhibits = SelectMultipleField(label='Inhibits',
+                                   id='inhibit-select',
                                    choices=[(c, c) for c in channels],
                                    validators=[validators.unicode_literals])
     not_inhibits = SelectMultipleField(label='Does not inhibit',
+                                       id='not-inhibit-select',
                                        choices=[(c, c) for c in channels],
                                        validators=[validators.unicode_literals])
     submit_button = SubmitField('Search')
@@ -27,13 +30,20 @@ def channel_search():
     print(inhibits)
     not_inhibits = request.form.getlist('not_inhibits')
     print(not_inhibits)
-    return str(inhibits) + str(not_inhibits)
+    response_list = list(set(inhibits).union(set(not_inhibits)))
+    session['response_list'] = response_list
+    return redirect('/')
 
 
 @app.route('/')
 def index():
     channel_search_form = ChannelSearchForm()
     kwargs = {'channel_search_form': channel_search_form}
+    if session.get('response_list'):
+        response_list = session.pop('response_list')
+    else:
+        response_list = []
+    kwargs['response_list'] = response_list
     return render_template('index.html', **kwargs)
 
 
