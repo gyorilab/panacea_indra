2406c1712ca2b84fcff0ba0dd7c04bb05ce636a4
Samuel Bunga <sambunga094@gmail.com>
2020-09-14 20:04:19 -0400

    saves interactions into a dataframe

--- panacea_indra/enzyme_client.py
+++ panacea_indra/enzyme_client.py
@@ -2,6 +2,7 @@ import os
 import re
 import logging
 import collections
+import pandas as pd
 from indra.ontology.bio import bio_ontology
 
 
@@ -47,28 +48,40 @@ def filter_human(enzyme_data):
 
 
 if __name__ == "__main__":
-    enzyme_file = os.path.join(os.path.dirname(os.path.abspath(__file__)),
-                               '../../data/enzyme.dat')
-    ed = load_enzyme_data(enzyme_file)
-    hd = filter_human(ed)
-    
     filtered_stmts = []
     with open(PATHWAY_HGNC_SIF) as fh:
         for lines in fh:
             if re.search("controls-production-of", lines):
                 filtered_stmts.append(lines.rstrip())
+    len(filtered_stmts)
 
     pain_signal_mol = {
-        "Prostaglandins" : "CHEBI:26333",
-        "Brandykinin" : "CHEBI:3165"
+        "Prostaglandins": "CHEBI:26333",
+        "Brandykinin": "CHEBI:3165"
     }
+
     chebi_list = collections.OrderedDict()
     for compounds, chebi_id in pain_signal_mol.items():
-        chebi_list[compounds] = [children[1] for children in 
-                                  bio_ontology.get_children('CHEBI', chebi_id)]
+        chebi_list[compounds] = [children[1] for children in
+                                 bio_ontology.get_children('CHEBI',
+                                                           chebi_id)]
+
     chebi_stmts = []
     for index, chebi_entries in chebi_list.items():
         for entry in chebi_entries:
             for stmts in filtered_stmts:
                 if re.search(entry, stmts):
-                    chebi_stmts.append(stmts)
+                    splitted_values = stmts.split("\t")
+                    chebi_name = bio_ontology.get_name('CHEBI',
+                                                       splitted_values[2])
+                    chebi_stmts.append(
+                        {
+                            'Enzymes': splitted_values[0],
+                            'Statement': splitted_values[1],
+                            'CHEBI_ID': splitted_values[2],
+                            'CHEBI_Name': chebi_name
+                        }
+                    )
+    df = pd.DataFrame(chebi_stmts)
+    df.to_csv("enzyme_interactions.tsv", sep="\t",
+              header=True, index=False)
