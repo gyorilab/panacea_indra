de5a48f670735f0f68e45f819b18dde1623cfaff
Ben Gyori <ben.gyori@gmail.com>
2020-09-15 23:25:47 -0400

    Clean up enzyme script

--- panacea_indra/enzyme_client.py
+++ panacea_indra/enzyme_client.py
@@ -1,25 +1,20 @@
-import os
 import re
-import logging
-import collections
+import requests
 import pandas as pd
 from indra.ontology.bio import bio_ontology
 
 
-ENZYME_FILE = os.path.join(os.path.dirname(os.path.abspath(__file__)),
-                           './data/enzyme.dat')
-PATHWAY_HGNC_SIF = os.path.join(os.path.dirname(os.path.abspath(__file__)),
-                           './data/PathwayCommons12.Detailed.hgnc.sif')
+ENZYME_URL = 'ftp://ftp.expasy.org/databases/enzyme/enzyme.dat'
+PC_SIF_URL = ('https://www.pathwaycommons.org/archives/PC2/v12/'
+              'PathwayCommons12.Detailed.hgnc.sif.gz')
 
 
-def load_enzyme_data(filename):
-    with open(enzyme_file) as f:
-        lines = f.readlines()
-    enzyme_data = collections.OrderedDict()
+def load_enzyme_data(dat_str):
+    enzyme_data = {}
     enzyme_class = None
     enzyme_entries = []
-    for line in lines:
-        g = re.match('^ID\s+([0-9\.]+)$', line)
+    for line in dat_str.split('\n'):
+        g = re.match(r'^ID\s+([0-9\.]+)$', line)
         if g:
             enzyme_class = g.groups()[0]
             enzyme_entries = []
@@ -40,7 +35,7 @@ def load_enzyme_data(filename):
 
 
 def filter_human(enzyme_data):
-    human_data = collections.OrderedDict()
+    human_data = {}
     for enz_class, enz_entries in enzyme_data.items():
         human_entries = [e for e in enz_entries if e[1].endswith('_HUMAN')]
         human_data[enz_class] = human_entries
@@ -49,39 +44,26 @@ def filter_human(enzyme_data):
 
 if __name__ == "__main__":
     filtered_stmts = []
-    with open(PATHWAY_HGNC_SIF) as fh:
-        for lines in fh:
-            if re.search("controls-production-of", lines):
-                filtered_stmts.append(lines.rstrip())
-    len(filtered_stmts)
+    df = pd.read_csv(PC_SIF_URL, sep='\t', header=None)
+    df = df[df[1] == 'controls-production-of']
 
     pain_signal_mol = {
         "Prostaglandins": "CHEBI:26333",
         "Brandykinin": "CHEBI:3165"
     }
 
-    chebi_list = collections.OrderedDict()
+    chebi_list = {}
     for compounds, chebi_id in pain_signal_mol.items():
         chebi_list[compounds] = [children[1] for children in
                                  bio_ontology.get_children('CHEBI',
                                                            chebi_id)]
-
-    chebi_stmts = []
-    for index, chebi_entries in chebi_list.items():
-        for entry in chebi_entries:
-            for stmts in filtered_stmts:
-                if re.search(entry, stmts):
-                    splitted_values = stmts.split("\t")
-                    chebi_name = bio_ontology.get_name('CHEBI',
-                                                       splitted_values[2])
-                    chebi_stmts.append(
-                        {
-                            'Enzymes': splitted_values[0],
-                            'Statement': splitted_values[1],
-                            'CHEBI_ID': splitted_values[2],
-                            'CHEBI_Name': chebi_name
-                        }
-                    )
+    df = df[df[2].isin(chebi_list)]
+    chebi_stmts = [
+        {'Enzyme': row[0],
+         'Statement': row[1],
+         'CHEBI_ID': row[2],
+         'CHEBI_Name': bio_ontology.get_name('CHEBI', row[2])}
+        for _, row in df.iterrows()]
     df = pd.DataFrame(chebi_stmts)
     df.to_csv("enzyme_interactions.tsv", sep="\t",
               header=True, index=False)
