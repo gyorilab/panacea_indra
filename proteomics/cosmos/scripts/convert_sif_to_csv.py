cfcbe29963f15bc1e551a03b2fe6984819d6ffee
Samuel Bunga <sambunga094@gmail.com>
2021-07-12 12:39:57 -0400

    modified sif input

--- proteomics/cosmos/scripts/convert_sif_to_csv.py
+++ proteomics/cosmos/scripts/convert_sif_to_csv.py
@@ -8,8 +8,8 @@ wd = __file__
 INDRA SIF location on S3:
 s3://bigmech/indra-db/dumps/2021-01-26/sif.pkl
 '''
-INDRA_SIF = os.path.join(os.pardir, 'input', 'sif.pkl')
+INDRA_SIF = os.path.join(os.pardir, 'input', 'indra_phospho_df.pkl')
 with open(INDRA_SIF, 'rb') as fh:
     SIF = pickle.load(fh)
-SIF.to_csv(os.path.join(os.pardir, 'output', 'indra_sif.csv'), 
+SIF.to_csv(os.path.join(os.pardir, 'output', 'indra_phospho_df.csv'), 
            index=0)
