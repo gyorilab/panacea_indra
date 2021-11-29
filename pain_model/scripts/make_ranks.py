d1f4cb3170da1bd4f68ad6be70eed98eb785980c
Samuel Bunga <sambunga094@gmail.com>
2021-11-29 16:06:01 -0500

    merged all the gene pct tables

--- pain_model/scripts/make_ranks.py
+++ pain_model/scripts/make_ranks.py
@@ -1,6 +1,7 @@
 import os
 import re
 import pandas as pd
+from sklearn.preprocessing import StandardScaler
 
 
 HERE = os.path.realpath(os.path.dirname(__file__))
@@ -8,7 +9,6 @@ INPUT = os.path.join(HERE, os.pardir, 'input')
 OUTPUT = os.path.join(HERE, os.pardir, 'output')
 
 # Read all the transcriptomics files
-
 files = {
     'motor_neuron': os.path.join(OUTPUT, 'gene_pct_motor_neuron_clusters.csv'),
     'heart': os.path.join(OUTPUT, 'gene_pct_heart_clusters.csv'),
@@ -20,3 +20,15 @@ files = {
 # Check if all the files exists
 all(True for k, v in files.items() if os.path.isfile(files[k]))
 
+all_df = {}
+for k, v in files.items():
+    df = pd.read_csv(v)
+    df.drop(df.columns[[1, 2, 3]], axis=1, inplace=True)
+    columns = [k+'_'+c for c in df.columns if c != 'MOUSE_SYMBOL']
+    columns.insert(0, 'MOUSE_SYMBOL')
+    df.columns = columns
+    all_df[k] = df
+
+df_merged = reduce(lambda left, right: pd.merge(left, right, on=['MOUSE_SYMBOL'],
+                                                how='outer'), all_df.values()).fillna(0)
+df_merged.to_csv(os.path.join(OUTPUT, 'all_gene_pct.csv'), index=False)
